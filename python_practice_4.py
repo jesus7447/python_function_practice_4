15e01e306cbdb83917a98c5d62e03b975bb9cd5c
User <j_camacho8@yahoo.com>
2023-01-12 14:35:14 -0800

    completed problem 2

--- python_practice_4.py
+++ python_practice_4.py
@@ -1,3 +1,15 @@
+#Write a Python function called max_num()to find the maximum of three numbers
 def max_num(a,b,c):
     return max([a,b,c])
-print(max_num(20,80,19))
+print(max_num(20,8,19))
+
+#Write a Python function called mult_list() to multiply all the numbers in a list.
+def mult_list(num):
+    
+    if num == 1:
+        return 1
+    return (num * mult_list(num - 1))
+n=10
+print(mult_list(n))
+
+#Write a Python function called rev_string() to reverse a string.
