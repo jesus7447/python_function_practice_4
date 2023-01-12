27f15cc51e5a5e9009384c2865f840960827e7b7
User <j_camacho8@yahoo.com>
2023-01-12 14:48:55 -0800

    completed fourth function

--- python_practice_4.py
+++ python_practice_4.py
@@ -18,3 +18,12 @@ def rev_string(x):
 
 string = rev_string('I am backwards')
 print(string)
+
+#Write a Python function called num_within() to check whether a number falls in a given range.
+def num_within(a,b,c):
+    return a in range(b,c+1)
+
+print(num_within(3,2,4))
+print(num_within(3,1,3))
+print(num_within(10,2,5))
+
